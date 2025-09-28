ead65c4ed8c5cc917e2590f2c3eeab01a264e48d
yao-eu <yaojiaeu@gmail.com>
2025-09-29 04:47:46 +0800

    Update video-test.js

--- video-test.js
+++ video-test.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // 3️⃣ 添加 source
   const source = document.createElement("source");
-  // 这里可以用你的真实视频路径(必须是可直接访问的 mp4/webm 直链)
+  // 这里可以用你的真实视频路径(必须是可直接访问的 mp4/webm 直链
   source.src = "https://player.vimeo.com/progressive_redirect/playback/1112811167/rendition/1080p/file.mp4?loc=external&log_user=0&signature=a4d204cc37b54fe7f3c721934c5ee005f6ba904c514ef654a9440b2296b6d0b4";
   source.type = "video/mp4";
 
